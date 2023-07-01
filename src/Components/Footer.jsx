e1225dc4977a1553f78e0693687b4f36f2cdd3c5
crxstian <cristian19overdrive@gmail.com>
2023-07-01 11:16:49 -0300

    Vercel social-media-icons

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -5,14 +5,14 @@ const Footer = () => {
     <footer>
         <div className="DH">
           <p>Powered by</p>
-          <img className="DH-img" src="../public/images/DH.png" alt='DH-logo' />
+          <img className="DH-img" src="images/DH.png" alt='DH-logo' />
         </div>
 
           <div>
-          <img className="social-media" src="../public/images/ico-facebook.png" alt="Facebook-logo" />
-          <img className="social-media" src="../public/images/ico-instagram.png" alt="Instagram-logo" />
-          <img className="social-media" src="../public/images/ico-tiktok.png" alt="TikTok-logo" />
-          <img className="social-media" src="../public/images/ico-whatsapp.png" alt="Whatsapp-logo" />  
+          <img className="social-media" src="images/ico-facebook.png" alt="Facebook-logo" />
+          <img className="social-media" src="images/ico-instagram.png" alt="Instagram-logo" />
+          <img className="social-media" src="images/ico-tiktok.png" alt="TikTok-logo" />
+          <img className="social-media" src="images/ico-whatsapp.png" alt="Whatsapp-logo" />  
           </div>
           
               
